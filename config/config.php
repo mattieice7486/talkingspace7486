8465f62736e92cf55aca1988628d540cbf453d6d
Matthew Eisenstadter <mattieice@gmail.com>
2019-05-09 10:53:20 -0400

    database update

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 //DB Params
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_HOST", "cleardb-octagonal-47930");
+define("DB_USER", "b05c9cd1741325");
+define("DB_PASS", "4fd4135c");
 define("DB_NAME", "talkingspace");
 
 define("SITE_TITLE", "Welcome To TalkingSpace!");
