51f01007e37aeb09260f4913175ca8d67dab8a11
Matthew Eisenstadter <mattieice@gmail.com>
2019-05-09 13:45:39 -0400

    updated database credentials

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 //DB Params
-define("DB_HOST", "mysql://b05c9cd1741325:4fd4135c@us-cdbr-iron-east-02.cleardb.net/heroku_c67bcc44057f343?reconnect=true");
+define("DB_HOST", "us-cdbr-iron-east-02.cleardb.net");
 define("DB_USER", "b05c9cd1741325");
 define("DB_PASS", "4fd4135c");
-define("DB_NAME", "talkingspace");
+define("DB_NAME", "heroku_c67bcc44057f343");
 
 define("SITE_TITLE", "Welcome To TalkingSpace!");
 
